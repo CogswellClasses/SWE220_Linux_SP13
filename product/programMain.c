b73d649d1acf4a338afef7ba4b85083d8ddc11e3
Cogswell <cogswell@ubuntu.(none)>
2013-02-27 10:56:08 -0800

    "The two files in the product folder have been uploaded. It has the program and a text file"

---
+++ product/programMain.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+char stringArray[100][80]; /*100 lines by 80 columns*/
+FILE *aphorismsFile;
+
+
+void main (int argc, char *argv[])
+{
+	int n = 0;
+
+	aphorismsFile = fopen("aphorismsFile.txt", "r");
+
+	while (!feof(aphorismsFile))
+	{
+		fgets(strings[n], 80, aphorismsFile);
+		printf("String read: %s\n", stringArray[n]);
+		n++;
+	}
+
+	fclose(aphorismsFile);
+	printf("\n\n");
+
+	int i = 0;
+
+	srand (time (NULL));
+	i = rand() % n;
+
+	printf("RANDOM STRING: %s", stringArray[i]);
+	printf ("\n\n");
+}
