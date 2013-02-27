d4bc7fdbdb281631c4ddc7f4d159ae2d29676854
Cogswell <cogswell@ubuntu.(none)>
2013-02-27 11:39:38 -0800

    "New programMain that uses a hardcoded array"

--- product/programMain.c
+++ product/programMain.c
@@ -2,31 +2,22 @@
 #include <stdlib.h>
 #include <time.h>
 
-char stringArray[100][80]; /*100 lines by 80 columns*/
-FILE *aphorismsFile;
+char *strings[] = {
+"here today, gone tomorrow",
+"a word to the wise is sufficient",
+"no matter where you go, there your are",
+NULL};
 
+char *saying () {
+int n;
+for (n = 0; strings[n]; n++);
+int i;
+srand (time (NULL));
+i = rand() % n;
+return strings[i];
+}
 
-void main (int argc, char *argv[])
-{
-	int n = 0;
-
-	aphorismsFile = fopen("aphorismsFile.txt", "r");
-
-	while (!feof(aphorismsFile))
-	{
-		fgets(stringArray[n], 80, aphorismsFile);
-		printf("String read: %s\n", stringArray[n]);
-		n++;
-	}
-
-	fclose(aphorismsFile);
-	printf("\n\n");
-
-	int i = 0;
-
-	srand (time (NULL));
-	i = rand() % n;
-
-	printf("RANDOM STRING: %s", stringArray[i]);
-	printf ("\n\n");
+void main (int argc, char *argv[]) {
+printf (saying ());
+printf ("\n");
 }
