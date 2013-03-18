014e8ce79aad4f1fce603fe03f8dc4b8fa31accc
David Gladstein <david@david-VirtualBox.(none)>
2013-03-18 10:11:59 -0700

    aphorismServer

---
+++ aphorismServer/product/programMain.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+char *strings[] = {
+#include "Read me"
+  NULL
+};
+
+char *saying () {
+  int n;
+  for (n = 0; strings[n]; n++);
+  int i;
+  srand (time (NULL));
+  i = rand() % n;
+  return strings[i];
+}
+
+void main (int argc, char *argv[]) {
+  printf (saying ());
+  printf ("\n");
+}
