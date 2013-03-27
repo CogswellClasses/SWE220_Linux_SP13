3a2848f018482dfe120786349485fd6f4a3e405f
Tanner <tanner@tanner-VirtualBox.(none)>
2013-03-27 11:57:48 -0700

    edited simple-server.c

--- aphorismServer/product/simple-server.c
+++ aphorismServer/product/simple-server.c
@@ -1,21 +1,3 @@
-/* simple-server.c
- *
- * Copyright (c) 2000 Sean Walton and Macmillan Publishers.  Use may be in
- * whole or in part in accordance to the General Public License (GPL).
- *
- * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
-*/
-
 /*****************************************************************************/
 /*** simple-server.c                                                       ***/
 /***                                                                       ***/
@@ -45,7 +27,7 @@ int main(int Count, char *Strings[])
 	char buffer[MAXBUF];
 
 	/*---Create streaming socket---*/
-    if ( (sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0 )
+    if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 )
 	{
 		perror("Socket");
 		exit(errno);
@@ -66,28 +48,19 @@ int main(int Count, char *Strings[])
 		exit(errno);
 	}
 
-	/*---Make it a "listening socket"---*/
-	if ( listen(sockfd, 20) != 0 )
-	{
-		perror("socket--listen");
-		exit(errno);
-	}
 
 	/*---Forever... ---*/
 	while (1)
-	{	int clientfd;
-		struct sockaddr_in client_addr;
+	{	struct sockaddr_in client_addr;
 		int addrlen=sizeof(client_addr);
-
-		/*---accept a connection (creating a data pipe)---*/
-		clientfd = accept(sockfd, (struct sockaddr*)&client_addr, &addrlen);
+		
+		recvfrom (sockfd, buffer, sizeof(buffer), 0,
+			(struct sockaddr*) &client_addr, &addrlen);
 		printf("%s:%d connected\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
 
 		strcpy (buffer, saying ());
-		send(clientfd, buffer, strlen (buffer), 0);
-
-		/*---Close data connection---*/
-		close(clientfd);
+		sendto(sockfd, buffer, strlen (buffer), 0,
+			(struct sockaddr*) &client_addr, addrlen);
 	}
 
 	/*---Clean up (should never get here!)---*/
