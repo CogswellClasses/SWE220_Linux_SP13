f946032f2a2bf971cb3cbdb1bce20cf00735c832
David Gladstein <david@hydra.(none)>
2013-04-22 10:11:02 -0700

    database

--- geoServer/cgi-bin/test.py
+++ geoServer/cgi-bin/test.py
@@ -1,4 +1,44 @@
 #!/usr/bin/python
 import cgi
-cgi.test ()
+import time
+import sqlite3
+
+import cgitb
+cgitb.enable ()
+
+conn = sqlite3.connect ('geoServer.db', isolation_level=None)
+
+#cgi.test ()
+
+print "Content-Type: text/html"     # HTML is following
+print                               # blank line, end of headers
+
+form = cgi.FieldStorage ()
+now = time.time ()
+
+print "As of %s user %s is at latitude %s longitude %s" % (time.ctime (now),
+                                                           form["user"].value,
+                                                           form["latitude"].value,
+                                                           form["longitude"].value
+                                                           )
+conn.execute ('''insert into geodata
+                 (timepoint,
+                  latitude,
+                  longitude, 
+                  username)
+                 values (?, ?, ?, ?)''',
+              (now,
+               form["user"].value,
+               form["longitude"].value,
+               form["latitude"].value)
+              )
+[[n]] = conn.execute ('select count(*) from geodata')
+
+
+print "<p>There are now %d records in the database:<p>" % n
+
+for row in conn.execute ('select * from geodata order by username, timepoint'):
+    (timepoint, latitude, longitude, username) = row
+    print time.ctime (timepoint), latitude, longitude, username, "<br>"
+
 
