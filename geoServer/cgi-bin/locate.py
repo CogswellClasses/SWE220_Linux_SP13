e1bcdc4fa69de1978bef393a5e553fae28abc4be
Aakash <aakash.trivedi1992@gmail.com>
2013-04-24 18:42:22 +0000

    yay! it worked

---
+++ geoServer/cgi-bin/locate.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+import cgi
+import time
+import sqlite3
+
+import cgitb
+cgitb.enable ()
+
+conn = sqlite3.connect ('geoServer.db', isolation_level=None)
+
+# cgi.test ()
+
+print "Content-Type: text/plain"
+print 
+
+for row in conn.execute ('select * from geodata order by username, timepoint'):
+        (timepoint, latitude, longitude, username) = row
+        print '\t'.join (map (str, [time.strftime  ("%c",
+						    time.localtime (timepoint)),
+				    latitude,
+				    longitude,
+				    username]))
